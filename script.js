728dcf262ac4f6028c248ba8cc3038caecb34203
eskaerpe <eskaerpe01@gmail.com>
2022-05-22 22:01:52 +0700

    penamaan class otomatis

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function submitForm() {
     var clone = template.content.cloneNode(true);
     clone.querySelector(".card-title").textContent = data.title;
     clone.querySelector(".card-text").textContent = data.description;
+
+    clone.querySelector(".thecard").classList.add(`thecard-${cardNumber}`);
+
     clone.querySelector(".thefront").style.backgroundColor = document.querySelector("#front-color").value;
     clone.querySelector(".theback").style.backgroundColor = document.querySelector("#back-color").value;
     clone.querySelector(".card-text").style.color = document.querySelector("#text-color").value;
