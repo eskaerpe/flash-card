8d487a290a6b3cc01b0bef090358d3041a1e18bb
eskaerpe <eskaerpe01@gmail.com>
2022-05-22 22:04:25 +0700

    menambahkan command penjelasan

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ document.querySelector("#text-color").value = "#333";
 var cardNumber = 0;
 
 function submitForm() {
+    // adding card number
     cardNumber += 1;
+
+    // fetching data from the form
     var titleValue = document.getElementById("title").value;
     var descriptionValue = document.getElementById("description").value;
     var data = {
@@ -13,16 +16,21 @@ function submitForm() {
     };
     console.log(data, "\nOne card created");
 
+    // creating a new card (clone from template)
     var template = document.querySelector("#card-template");
     var clone = template.content.cloneNode(true);
     clone.querySelector(".card-title").textContent = data.title;
     clone.querySelector(".card-text").textContent = data.description;
 
+    // adding specific class name to the card
     clone.querySelector(".thecard").classList.add(`thecard-${cardNumber}`);
 
+    // adding card style by user request from the form
     clone.querySelector(".thefront").style.backgroundColor = document.querySelector("#front-color").value;
     clone.querySelector(".theback").style.backgroundColor = document.querySelector("#back-color").value;
     clone.querySelector(".card-text").style.color = document.querySelector("#text-color").value;
     clone.querySelector(".card-title").style.color = document.querySelector("#text-color").value;
+
+    // apply all change to the page
     document.querySelector("#card-place").append(clone);
 }
