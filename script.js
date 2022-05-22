4ec7143719200a8fd229ffd52f44ac4dee3b03ae
eskaerpe <eskaerpe01@gmail.com>
2022-05-22 21:36:57 +0700

    menambahkan fitur ganti warna

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+document.querySelector("#front-color").value = "#67a4f3";
+document.querySelector("#back-color").value = "#f3bb67";
+document.querySelector("#text-color").value = "#333";
+var cardNumber = 0;
+
 function submitForm() {
+    cardNumber += 1;
     var titleValue = document.getElementById("title").value;
     var descriptionValue = document.getElementById("description").value;
     var data = {
@@ -11,5 +17,9 @@ function submitForm() {
     var clone = template.content.cloneNode(true);
     clone.querySelector(".card-title").textContent = data.title;
     clone.querySelector(".card-text").textContent = data.description;
+    clone.querySelector(".thefront").style.backgroundColor = document.querySelector("#front-color").value;
+    clone.querySelector(".theback").style.backgroundColor = document.querySelector("#back-color").value;
+    clone.querySelector(".card-text").style.color = document.querySelector("#text-color").value;
+    clone.querySelector(".card-title").style.color = document.querySelector("#text-color").value;
     document.querySelector("#card-place").append(clone);
 }
